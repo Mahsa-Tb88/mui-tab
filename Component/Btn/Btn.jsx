d5dc743acc38cfeb013c0eccc479c4aa756a2d00
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2023-08-08 02:15:07 +0330

    add style and complete project

---
+++ Component/Btn/Btn.jsx
@@ -0,0 +1,19 @@
+import style from "./Btn.module.css";
+const Btn = ({ btn, clickHandler, dataBtn }) => {
+  {
+    console.log(dataBtn,btn.id);
+  }
+  return (
+    <div>
+      <button
+        key={btn.id}
+        onClick={clickHandler}
+        className={dataBtn.id == btn.id ? style.active : style.btn}
+      >
+        {btn.title}
+      </button>
+    </div>
+  );
+};
+
+export default Btn;
--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,34 @@
 import { useState } from "react";
-import Btns from "../Component/Btns/Btns";
+import Btn from "../Component/Btn/Btn";
+import "./App.css";
 
 const App = () => {
   const tabDate = [
-    { id: 1, title: "item1", content: "this is item1 content use it" },
-    { id: 2, title: "item2", content: "this is item2 content use it" },
-    { id: 3, title: "item3", content: "this is item3 content use it" },
+    { id: 1, title: "Item 1", content: "--- this is item1 content use it ---" },
+    { id: 2, title: "Item 2", content: "### this is item2 content use it ###" },
+    { id: 3, title: "Item 3", content: "%%% this is item3 content use it %%%" },
   ];
-  const [idBtn, setIdBtn] = useState({ id: 1, content: tabDate[0].content });
+  const [dataBtn, setDataBtn] = useState({
+    id: 1,
+    content: tabDate[0].content,
+  });
   const clickHandler = (id) => {
-    const data = tabDate.filter((tab) => tab.id == id);
-    setIdBtn({ id: id, content: data[0].content });
+    const selectedBtn = tabDate.filter((tab) => tab.id == id);
+    setDataBtn({ id: id, content: selectedBtn[0].content });
   };
   return (
-    <div>
-      {tabDate.map((btn) => {
-        return (
-          <div key={btn.id}>
-            <Btns btns={btn} clickHandler={() => clickHandler(btn.id)} />
-          </div>
-        );
-      })}
-      <p>{idBtn.content}</p>
+    <div className="box">
+      <div className="btns">
+        {tabDate.map((btn) => {
+          return (
+            <div key={btn.id}>
+               <Btn btn={btn} clickHandler={() => clickHandler(btn.id)} dataBtn={dataBtn}/> 
+            </div>
+          );
+        })}
+      </div>
+
+      <p>{dataBtn.content}</p>
     </div>
   );
 };
